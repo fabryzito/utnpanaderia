a5bbf076440aef67c884969e6412563d1562b033
FrancoVieytes <francovieytes2004@gmail.com>
2025-09-03 17:08:03 -0300

    se agrego variables de entorno

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:3001";
+const API = import.meta.env.VITE_API_URL;
 
 export const getProductos = () => axios.get(`${API}/productos`);
 export const getProducto = (id) => axios.get(`${API}/productos/${id}`);
